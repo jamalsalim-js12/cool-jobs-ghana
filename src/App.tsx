5f25e0ced80c88bf84f6d8826b25ec3079c0b51e
Jamal Salim <jamalsalim.js12@gmail.com>
2024-06-22 17:09:40 +0000

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+const App = () => {
+  return (
+    <div className="m-5">
+      <Button size="default">Click me</Button>
+    </div>
+  );
+};
+
+export default App;
